f00cfcec9d42d0140737cbdad45d0f52e729acf9
Robert Coffie <rcoffie22@yhahoo.com>
2022-10-21 17:02:16 +0000

    unit testing account view

--- leads/test/test_account.py
+++ leads/test/test_account.py
@@ -1,5 +1,8 @@
 from rest_framework.test import APITestCase
 from leads.models_user import User, Account
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 class TestAccountModel(APITestCase):
     def setUp(self):
@@ -10,11 +13,48 @@ class TestAccountModel(APITestCase):
         self.assertEqual(self.account.name, 'name')
         self.assertEqual(str(self.account), 'name')
 
-
+# the below test are same test but with different approach 
 class TestAccountView(APITestCase):
     def setUp(self):
+        self.factory = APIRequestFactory()
         BASE_URL = 'http://localhost:8000'
         ACCOUNT_URL = BASE_URL + '/api/account'
+        self.account_url = ACCOUNT_URL,
+        self.user = User.objects.create_user( email="test@mail.com", password="p12345678")
 
     def test_create_account(self):
-        pass
+
+        user = self.user
+        self.client.force_login(user)
+        data = {
+        "name":"name",
+        "subdomain":"name"
+        }
+        response = self.client.post('/api/account', data)
+        self.assertEqual(response.status_code, 403)
+
+    def test_authenticated_user_can_see_account(self):
+        user= User.objects.create_user("a@a.com", "somepass")
+        self.client.force_login(user=user)
+        response = self.client.get("/api/account")
+        self.assertEqual(response.status_code, 200)
+
+class TryingAuthentication(APITestCase):
+    def setUp(self):
+        self.login_url = '/api/user'
+        self.user_data = {
+        "email":"a@a.com",
+        "password":"pass"
+
+        }
+        self.client.post(self.login_url, self.user_data)
+        auth_url = '/api/user/login'
+        self.access_token = self.client.post(auth_url, {
+        "email":self.user_data.get("email"),
+        "password": self.user_data.get("password")
+        }).data.get("access")
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+    def test_account_page(self):
+        response = self.client.get('/api/account')
+        self.assertEqual(response.status_code, 200)
