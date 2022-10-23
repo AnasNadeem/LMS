b07cffa04e70c944c8fb727f23b139ed7ee53749
Robert Coffie <rcoffie22@yhahoo.com>
2022-10-23 15:03:27 +0000

    getting usser token in test

--- leads/test/test_account.py
+++ leads/test/test_account.py
@@ -3,6 +3,8 @@ from leads.models_user import User, Account
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from django.urls import reverse
+
 
 class TestAccountModel(APITestCase):
     def setUp(self):
@@ -15,46 +17,29 @@ class TestAccountModel(APITestCase):
 
 # the below test are same test but with different approach 
 class TestAccountView(APITestCase):
+    print('start test')
     def setUp(self):
-        self.factory = APIRequestFactory()
-        BASE_URL = 'http://localhost:8000'
-        ACCOUNT_URL = BASE_URL + '/api/account'
-        self.account_url = ACCOUNT_URL,
-        self.user = User.objects.create_user( email="test@mail.com", password="p12345678")
+        #create authenticated user 
+        self.base_url = 'http://localhost:8000'
+        self.register_url = self.base_url + '/api/user'
+        self.login_url = self.base_url + '/api/user/login'
+        self.account_url = self.base_url + '/api/account'
+        self.user = User.objects.create_user("email" "test@test.com","password" "testpassword")
+        client = APIClient
 
-    def test_create_account(self):
+    def test_list_account_view(self):
+        print("list account view test starts from here")
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        self.client.login(user=self.user)
+        response = self.client.get(self.account_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        user = self.user
-        self.client.force_login(user)
-        data = {
-        "name":"name",
-        "subdomain":"name"
-        }
-        response = self.client.post('/api/account', data)
-        self.assertEqual(response.status_code, 403)
+        
 
-    def test_authenticated_user_can_see_account(self):
-        user= User.objects.create_user("a@a.com", "somepass")
-        self.client.force_login(user=user)
-        response = self.client.get("/api/account")
-        self.assertEqual(response.status_code, 200)
+        
+        
+        
 
-class TryingAuthentication(APITestCase):
-    def setUp(self):
-        self.login_url = '/api/user'
-        self.user_data = {
-        "email":"a@a.com",
-        "password":"pass"
-
-        }
-        self.client.post(self.login_url, self.user_data)
-        auth_url = '/api/user/login'
-        self.access_token = self.client.post(auth_url, {
-        "email":self.user_data.get("email"),
-        "password": self.user_data.get("password")
-        }).data.get("access")
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-
-    def test_account_page(self):
-        response = self.client.get('/api/account')
-        self.assertEqual(response.status_code, 200)
+        
+        
