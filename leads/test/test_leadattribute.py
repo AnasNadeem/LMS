a70bb5a0f4a82ebea4575e5c24cb6d22e3341af4
Robert Coffie <rcoffie22@yhahoo.com>
2022-10-30 08:06:38 +0000

    uinit testing lead_attribute and working on code readability

---
+++ leads/test/test_leadattribute.py
@@ -0,0 +1,36 @@
+from leads.models_user import User, UserOTP
+from leads.models_lead import Lead
+from .test_base import ConstantMixin
+from rest_framework.test import APITestCase
+
+
+class TestLeadAttribute(APITestCase, ConstantMixin):
+    def setUp(self):
+        self.email = "test@test.com"
+        self.password = "password"
+        self.user = User.objects.create_user(email=self.email, password=self.password)
+
+    # test un authorize user
+    def test_get_lead_attribute_without_auth(self):
+        lead_attr_resp = self.client.get(self.LEAD_ATTR_URL)
+        self.assertEqual(lead_attr_resp.status_code, 403)
+
+    def test_get_list_with_auth(self):
+        # sign up
+        registration_resp = self.client.post(self.REGISTER_URL, self.USER_DATA)
+        self.assertEqual(registration_resp.status_code, 200)
+
+        # User Otp
+        user_otp = UserOTP.objects.all().first()
+        user_otp.is_verified = True
+        user_otp.save()
+
+        # login
+        login_resp = self.client.post(self.LOGIN_URL, self.USER_DATA)
+        token = login_resp.json()["token"]
+        self.client.credentials(HTTP_AUTHORIZATION=token)
+
+        # Accessing the url
+        # this is return 403 insted of 200
+        lead_attr_resp = self.client.get(self.LEAD_ATTR_URL)
+        self.assertEqual(lead_attr_resp.status_code, 200)
