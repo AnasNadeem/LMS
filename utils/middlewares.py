eece2c19dd6ad9a20e0d6f987489b05aee77bf46
Anas Nadeem <anas5678go@gmail.com>
2022-12-21 23:54:01 +0530

    Updated AccountMiddleware

--- utils/middlewares.py
+++ utils/middlewares.py
@@ -1,8 +1,7 @@
 from django.contrib.sites.models import Site
-from django.http import Http404
 
 from utils.jwtauth import JWTAuthentication
-from leads.models_user import Account, Member
+from leads.models_user import Member
 
 
 class AccountMiddleware(object):
@@ -21,25 +20,12 @@ class AccountMiddleware(object):
             response = self.get_response(request)
             return response
 
-        hostname = request.get_host()
-        hostname_split_dot = hostname.split('.')
-        domain_split_dot = self.domain.split('.')
-
-        subdomain_split_dot = hostname_split_dot[:-len(domain_split_dot)]
-        if subdomain_split_dot:
-            subdomain = '.'.join(subdomain_split_dot)
-            request.account = Account.objects.filter(subdomain__iexact=subdomain).first()
-            request.member = request.user.member_set.all().first()
-        else:
-            request.member = Member.objects.filter(user=request.user).first() if request.user else None
-            request.account = request.member.account if request.member else None
+        request.member = Member.objects.filter(user=request.user).first()
+        request.account = request.member.account if request.member else None
 
         if not request.account:
             response = self.get_response(request)
             return response
-
-        if not getattr(request, 'member', None):
-            raise Http404(f"Member {request.user.email} not found")
 
         response = self.get_response(request)
         return response
