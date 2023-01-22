3e07c0da6b64c95201a3d04b8eb6609579e9a762
Anas Nadeem <anas5678go@gmail.com>
2023-01-22 22:16:15 +0530

    Deleted field from lead after deletion of leadattribute

--- leads/signals.py
+++ leads/signals.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from leads.models_user import Member
@@ -29,3 +29,10 @@ def add_field_to_existing_lead_data(sender, instance, created, **kwargs):
     # for lead in instance.account.lead_set.all():
     #     update_field = {f"data__{instance.lead_type}__{instance.slug}": None}
     #     lead.update(**update_field)
+
+
+@receiver(post_delete, sender=LeadAttribute)
+def delete_field_to_existing_lead_data(sender, instance, **kwargs):
+    for lead in Lead.objects.filter(account=instance.account):
+        del lead.data[f"{instance.lead_type}"][f"{instance.slug}"]
+        lead.save()
